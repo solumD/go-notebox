9f3cea409221ea6d814c864dfe060cb9cd3eb266
Dmitriy Kononov <dimkkononov@gmail.com>
2024-02-05 12:57:39 +0300

    Add home page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
@@ -14,9 +15,36 @@ func main() {
 	mux.HandleFunc("/note", showNote)
 	mux.HandleFunc("/note/create", createNote)
 
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
 
-//
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+	return f, nil
+}
